4913377313815086525357df0aa49b9852769194
oxygenplug <dan@dandotjs.com>
2022-12-05 16:54:59 -0600

    added queue and stack implementations

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,40 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
-
+    private head?: Node<T>;
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
-
-}
+        const node = { value: item } as Node<T>;
+        this.length++;
+        if (!this.head) {
+            this.head = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+    }
     pop(): T | undefined {
-
-}
+        this.length = Math.max(0, this.length - 1);
+        if (!this.length) {
+            const head = this.head;
+            this.head = undefined;
+            return head?.value;
+        } else {
+            const head = this.head;
+            this.head = this.head?.next;
+            return head?.value;
+        }
+        this.length--;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
